b8f402c87c5f01e855e283b4d7a4d6210c4db2af
yudongliangsh <45057482@qq.com>
2018-05-08 17:48:22 +0800

    add some test

--- test02.py
+++ test02.py
@@ -3,8 +3,27 @@ import numpy as np
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"]='2'
 
-a = tf.constant([[1,1],[1,1]])
-b = tf.constant([[1,2],[3,4]])
-sess = tf.Session()
+# create data
+x_data = np.random.rand(100).astype(np.float32)
+y_data = x_data * 0.1 + 0.3
 
-print(sess.run(tf.add(a,b)))
+#create model
+Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+biases = tf.Variable(tf.zeros([1]))
+
+y = Weights * x_data + biases
+
+loss = tf.reduce_mean(tf.square(y - y_data))
+
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+sess = tf.Session()
+sess.run(init)          # Very important
+
+for step in range(201):
+    sess.run(train)
+    if step % 20 == 0:
+        print(step, sess.run(Weights), sess.run(biases),sess.run(loss))
